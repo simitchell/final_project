5580e69a3284997638829be5897b17e26029a0b2
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-29 09:52:25 -0400

    React Router working

--- final_project_frontend/src/main.jsx
+++ final_project_frontend/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./routes/root";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      // {
+      //   path: "/",
+      //   element: <Home />,
+      // },
+      // {
+      //   path: "/",
+      //   element: <Login />,
+      // },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ final_project_frontend/src/routes/root.jsx
@@ -0,0 +1,10 @@
+import { useLoaderData, Link, Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+export default function Root() {
+  return (
+    <>
+      <h1>Hello from Root route</h1>
+    </>
+  );
+}
