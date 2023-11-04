99fd7b716b2398278c3cb0d16a3f5f695175cb7a
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-11-04 18:10:15 -0400

    added card styling

---
+++ final_project_frontend/src/components/GlobalStyles/CardStyle.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+export const CardContainer = styled.div`
+  .card {
+    display: flex;
+    flex-direction: column;
+    max-width: 600px;
+    border: none;
+    background-color: #e2e2e2;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    border-radius: 25px;
+    margin: 1rem;
+    padding: 1rem;
+
+    h2 {
+      margin-top: 0;
+    }
+
+    .cardInfo {
+      display: flex;
+      flex-direction: row;
+    }
+
+    .returnInfo {
+      display: flex;
+      flex-direction: column;
+      text-align: left;
+      padding: 1rem;
+
+      span {
+        margin-bottom: 1rem;
+      }
+    }
+  }
+`;
+
+// this is potentially the code for the wrapper the cards will display into
+// .cardDisplay {
+//     display: flex;
+//     flex-direction: row;
+//     flex-wrap: wrap;
+//     max-width: 1400px;
+//     margin: auto;
+//     padding: auto;
+//     justify-content: space-evenly;
+// }
--- final_project_frontend/src/components/ProfilePage.jsx
+++ final_project_frontend/src/components/ProfilePage.jsx
@@ -1,8 +1,9 @@
+import { CardContainer } from "./GlobalStyles/CardStyle";
 import { useState, useEffect } from "react";
 import React from "react";
 
 export default function ProfilePage() {
-//   const [info, setInfo] = useState("");
+  //   const [info, setInfo] = useState("");
   const [listingData, setListingData] = useState([]);
 
   const getInfo = async () => {
@@ -16,41 +17,43 @@ export default function ProfilePage() {
     getInfo();
   }, []);
 
-//   Only log listingData when you need to
-//   console.log(listingData);
+  //   Only log listingData when you need to
+  //   console.log(listingData);
 
   return (
-    <div>
+    <CardContainer>
       <p>Profile Page</p>
       {listingData.map((listing, index) => {
         console.log({ listing });
         return (
-          <div key={index}>
+          <div key={index} className="card">
             <h2>{listing.title}</h2>
-            <div>
-              <p>this will be an image</p>
-            </div>
-            <div>
-              <span>
-                <strong>Title: </strong>
-                {listing.title}
-              </span>
-              <span>
-                <strong>Price: </strong>
-                {listing.price}
-              </span>
-              <span>
-                <strong>Description: </strong>
-                {listing.description}
-              </span>
-              <span>
-                <strong>User: </strong>
-                {listing.user}
-              </span>
+            <div className="cardInfo">
+              <div>
+                <p>this will be an image</p>
+              </div>
+              <div className="returnInfo">
+                <span>
+                  <strong>Title: </strong>
+                  {listing.title}
+                </span>
+                <span>
+                  <strong>Price: </strong>
+                  {listing.price}
+                </span>
+                <span>
+                  <strong>Description: </strong>
+                  {listing.description}
+                </span>
+                <span>
+                  <strong>User: </strong>
+                  {listing.user}
+                </span>
+              </div>
             </div>
           </div>
         );
       })}
-    </div>
+    </CardContainer>
   );
 }
