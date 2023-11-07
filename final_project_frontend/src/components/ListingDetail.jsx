68d59368e86e32f6983c8a94b8612f4db2043fa0
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-11-07 14:30:00 -0500

    updated listing detail with some ternary loading stuff

--- final_project_frontend/src/components/ListingDetail.jsx
+++ final_project_frontend/src/components/ListingDetail.jsx
@@ -1,29 +1,81 @@
-import { CardContainer } from "./GlobalStyles/CardStyle";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import React from "react";
+import { CardContainer } from "./GlobalStyles/CardStyle";
+
+// const INITIAL_DATA = {
+//   description: string,
+//   id: number,
+//   price: number,
+//   title: string,
+//   user: number,
+//   username: string,
+// };
 
 export default function ListingDetail() {
-  const [listingData, setListingData] = useState();
+  const [listingDetail, setListingDetail] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const { id } = useParams();
   console.log(id);
 
-  const getInfo = async () => {
-    const apiUrl = `http://127.0.0.1:8000/listing/${id}/`;
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    setListingData(data);
-    console.log(data);
+  const getIndividualListing = async () => {
+    try {
+      const apiUrl = `http://127.0.0.1:8000/listing/${id}/`;
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setListingDetail(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
-    getInfo();
+    getIndividualListing();
   }, []);
 
   return (
     <>
-      <p>helloooo</p>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {listingDetail ? (
+            <CardContainer>
+              <div className="card">
+                <h2>{listingDetail.title}</h2>
+                <div className="cardInfo">
+                  <div>
+                    <p>this will be an image</p>
+                  </div>
+                  <div className="returnInfo">
+                    <span>
+                      <strong>Title: </strong>
+                      {listingDetail.title}
+                    </span>
+                    <span>
+                      <strong>Price: </strong>${listingDetail.price}
+                    </span>
+                    <span>
+                      <strong>Description: </strong>
+                      {listingDetail.description}
+                    </span>
+                    <span>
+                      <strong>User: </strong>
+                      {listingDetail.username}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </CardContainer>
+          ) : (
+            <div>no detail found</div>
+          )}
+        </div>
+      )}
     </>
   );
 }
