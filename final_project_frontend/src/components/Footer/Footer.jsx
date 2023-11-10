4f24753c24a06def431a2593904b46f0f3856b95
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-11-09 22:55:00 -0500

    last commit of the night

--- final_project_frontend/src/components/Footer/Footer.jsx
+++ final_project_frontend/src/components/Footer/Footer.jsx
@@ -4,43 +4,46 @@ import { StyleFooter } from "./StyleFooter";
 export default function Footer() {
   return (
     <StyleFooter>
-    <div className="main-footer">
-      <div className="container">
-        <div className="row">
-          {/* Column 1 */}
-          <div className="col">
-            <h4>FOX BODY SWAP MEET</h4>
-            <ul className="list-unstyled">
-              <li>(555)-555-5555</li>
-              <li>Greenville, SC</li>
-              <li>411 University Ridge</li>
-            </ul>
+      <div className="main-footer">
+        <div className="container">
+          <div className="row">
+            {/* Column 1 */}
+            <div className="col">
+              <h4>FOX BODY SWAP MEET</h4>
+              <ul className="list-unstyled">
+                <li>(555)-555-5555</li>
+                <li>Greenville, SC</li>
+                <li>411 University Ridge</li>
+              </ul>
+            </div>
+            {/* Column 2 */}
+            <div className="col">
+              <h4>5.0L</h4>
+              <ul className="list-unstyled">
+                <li>Windsor</li>
+                <li>T-5 Transmissions</li>
+                <li>A9L Engine Management</li>
+              </ul>
+            </div>
+            {/* Column 3 */}
+            <div className="col">
+              <h4>THiNgS</h4>
+              <ul className="list-unstyled">
+                <li>All</li>
+                <li>the</li>
+                <li>things</li>
+              </ul>
+            </div>
           </div>
-          {/* Column 2 */}
-          <div className="col">
-            <h4>5.0L</h4>
-            <ul className="list-unstyled">
-              <li>Windsor</li>
-              <li>T-5 Transmissions</li>
-              <li>A9L Engine Management</li>
-            </ul>
-          </div>
-          {/* Column 3 */}
-          <div className="col">
-            <h4>THiNgS</h4>
-            <ul className="list-unstyled">
-              <li>All</li>
-              <li>the</li>
-              <li>things</li>
-            </ul>
-          </div>
-        </div>
-        <div className="row2">
+          <div className="row2">
             <p>Contact us at: foxbodyswapmeet@gmail.com</p>
-            <p className="col-sm">&copy;{new Date().getFullYear()} FOX BODY SWAP MEET INC | ALL RIGHTS RESERVED | LLC</p>
+            <p className="col-sm">
+              &copy;{new Date().getFullYear()} FOX BODY SWAP MEET INC | ALL
+              RIGHTS RESERVED | LLC
+            </p>
+          </div>
         </div>
       </div>
-    </div>
     </StyleFooter>
   );
 }
--- final_project_frontend/src/components/GlobalStyles/StyleRoot.jsx
+++ final_project_frontend/src/components/GlobalStyles/StyleRoot.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const StyleRoot = styled.div`
-    max-width: 65vw;
-    margin: auto;
-    padding: 2rem;
-    h2 {
-        text-align: right;
-    }
-`
+  max-width: 65vw;
+  margin: auto;
+  padding: 2rem;
+  h2 {
+    text-align: right;
+  }
+`;
