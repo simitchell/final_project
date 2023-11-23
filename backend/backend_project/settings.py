4d544d6119ecd50e08d90b1516a1e8258396bd32
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-11-23 10:52:28 -0500

    adding more to CORS stuff

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
 
 # CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = "https://foxbodyswapmeet.com"
+CORS_ALLOWED_ORIGINS = "https://foxbodyswapmeet.com", "https://foxbodyswapmeet.netlify.app/"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
