cfc1561bade8704b21ab398f1a964052aec8fb8e
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-11-29 16:25:17 -0500

    trying a new way to navigate to /profile on login

--- final_project_frontend/src/components/LoginForm.jsx
+++ final_project_frontend/src/components/LoginForm.jsx
@@ -43,7 +43,7 @@ export default function LoginForm() {
       localStorage.setItem("userId", userId);
       localStorage.setItem("access_token", access);
       localStorage.setItem("refresh_token", refresh);
-      // navigate('/profile');
+      useNavigate("/profile");
       // console.log(userId);
     }
     if (access == undefined) {
