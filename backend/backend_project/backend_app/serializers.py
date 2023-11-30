5b843854409b2d0278bae44bd063dda1fc59f91c
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-11-30 10:52:43 -0500

    last commit before I try to put these images into the backend

--- backend/backend_project/backend_app/serializers.py
+++ backend/backend_project/backend_app/serializers.py
@@ -46,12 +46,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = ["id", "user_id", "bio", "address", "birthdate"]
         lookup_field = "user_id"
 
-    # def update(self, request):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     user = serializer.save()
-    #     return Response(serializer.data)
-
 
 class UserSerializer(serializers.ModelSerializer):
     # profile = ProfileSerializer()
@@ -59,8 +53,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "email", "username", "password"]
-
-
-# class RegisterSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model =
