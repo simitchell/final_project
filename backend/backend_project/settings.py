169f78ad3f62acef6666ad5720130fdcc1267eca
Sam Mitchell <samuel.i.mitchell@gmail.com>
2026-04-22 16:23:15 -0400

    configure DATABASE_URL for Railway

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -114,6 +114,12 @@ DATABASES = {
     )
 }
 
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(
+        env="DATABASE_URL",
+        conn_max_age=600,
+    )
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
