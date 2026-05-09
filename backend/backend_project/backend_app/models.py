b760e8d83bea18962c1682b989c3f7103bb3912a
Sam Mitchell <samuel.i.mitchell@gmail.com>
2026-05-09 10:25:00 -0400

    update models so that username displays in django backend

--- backend/backend_project/backend_app/models.py
+++ backend/backend_project/backend_app/models.py
@@ -45,7 +45,9 @@ class Profile(models.Model):
     address = models.CharField(max_length=50, blank=True)
     bio = models.TextField(max_length=400, blank=True)
     birthdate = models.DateField(null=True, blank=True)
-
+    
+    def __str__(self):
+        return self.user.username
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
